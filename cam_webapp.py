92db74c44248364974b5a5ed0ef0aba46441dfe4
vyasdhairya <70323148+vyasdhairya@users.noreply.github.com>
2021-07-09 00:36:17 +0530

    Update cam_webapp.py

--- cam_webapp.py
+++ cam_webapp.py
@@ -7,8 +7,6 @@ Created on Fri Jul  9 00:01:02 2021
 
 import cv2
 import streamlit as st
-cascPath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascPath)
 st.title("Webcam Live Feed")
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
@@ -16,17 +14,6 @@ camera = cv2.VideoCapture(0)
 
 while run:
     _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
     FRAME_WINDOW.image(frame)
 else:
     st.write('Stopped')
