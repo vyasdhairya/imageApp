b41e0667e453c2a02888f780abf8e60f0dc37b0f
vyasdhairya <70323148+vyasdhairya@users.noreply.github.com>
2021-07-09 00:48:19 +0530

    Update cam_webapp.py

--- cam_webapp.py
+++ cam_webapp.py
@@ -1,19 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  9 00:01:02 2021
-
-@author: Dhairya
-"""
-
-import cv2
 import streamlit as st
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
+from streamlit_webrtc import webrtc_streamer
 
-while run:
-    _, frame = camera.read()
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+webrtc_streamer(key="example")
